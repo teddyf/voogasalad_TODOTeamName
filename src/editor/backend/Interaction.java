9b4b7cc61c83d5a761d7ad3d8c68f49c723c6040
William Xiong <william.xiong@duke.edu>
2016-11-20 21:40:23 -0500

    filled out abstract interaction, blockstate, interaction

--- src/editor/backend/Interaction.java
+++ src/editor/backend/Interaction.java
@@ -3,4 +3,5 @@ package editor.backend;
 import javafx.scene.text.Text;
 
 public interface Interaction {
+    public void act();
 }
--- src/interactions/AbstractInteraction.java
+++ src/interactions/AbstractInteraction.java
@@ -1,6 +1,9 @@
 package interactions;
 
+import boardObjects.Block;
 import editor.backend.Interaction;
+import editor.backend.Item;
+import player.Player;
 
 /**
  * All interactions are to be used as classes that are going to be composed with board objects.
@@ -9,5 +12,15 @@ import editor.backend.Interaction;
  *
  * @author Filip Mazurek
  */
-public class AbstractInteraction implements Interaction{
+public abstract class AbstractInteraction implements Interaction{
+    private Item item;
+    private Block block;
+    private Player player;
+    public AbstractInteraction(Item item, Block block, Player player){
+        this.item = item;
+        this.block = block;
+        this.player = player;
+    }
+    
+    public abstract void act();
 }
