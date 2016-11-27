853d0fb815bbc55179c08dbf4c0fb4007df8cc3f
Filip Mazurek <fjm7@duke.edu>
2016-11-27 17:51:25 -0500

    Instead of a seperate ShallowBlock class, makes a ShallowBlock interface.

---
+++ src/boardObjects/ShallowBlock.java
@@ -0,0 +1,16 @@
+package boardObjects;
+
+/**
+ * A shallow block interface. This interface provides access to only the basic information within a block, and does not
+ * allow any modification access.
+ *
+ * @author Filip Mazurek
+ */
+public interface ShallowBlock {
+
+    int getRow();
+
+    int getColumn();
+
+    String getName();
+}
