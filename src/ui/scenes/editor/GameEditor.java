c8f21cc61f980e9596bc4a2d3cd8b5c7e025e9d6
Robert Steilberg <rsteilberg@gmail.com>
2016-11-28 17:20:22 -0500

    add menu bar

--- src/ui/scenes/editor/GameEditor.java
+++ src/ui/scenes/editor/GameEditor.java
@@ -1,13 +1,8 @@
 package ui.scenes.editor;
 
-import grid.*;
-import editor.SidePanel;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.effect.ColorAdjust;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import ui.UILauncher;
 import ui.builder.UIBuilder;
@@ -27,13 +22,10 @@ public class GameEditor extends Scene {
     private UILauncher myLauncher;
     private Parent myRoot;
     private UIBuilder myBuilder;
-    private Group mySideMenuRegion;
     private ResourceBundle myResources;
-    private GridPane myGridPane;
-    private SidePanel sideMenu;
 
     public GameEditor(Stage stage, UILauncher launcher, Parent root) {
-        super(root);
+        super(root,Color.web("#4B84AF"));
         myStage = stage;
         myLauncher = launcher;
         myRoot = root;
@@ -51,8 +43,10 @@ public class GameEditor extends Scene {
      */
     public void initEditor() {
         myBuilder.initWindow(myStage, EDITOR_RESOURCES);
-        ItemMenuUI itemMenu = new ItemMenuUI(myRoot, myBuilder, EDITOR_RESOURCES);
+        MenuBarUI menuBar = new MenuBarUI(myStage,myRoot,EDITOR_RESOURCES);
+        menuBar.initMenuBar();
 
+        ItemMenuUI itemMenu = new ItemMenuUI(myRoot, myBuilder, EDITOR_RESOURCES);
         GridUI grid = new GridUI(myRoot, itemMenu.initItemMenu(), EDITOR_RESOURCES);
         grid.initGrid();
     }
