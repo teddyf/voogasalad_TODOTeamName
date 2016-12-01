b2958b65525eec9dc1efcb6b6d7b2a8104508fc1
Robert Steilberg <rsteilberg@gmail.com>
2016-12-01 00:06:59 -0500

    begin add design to side panel

--- src/ui/scenes/editor/GridUI.java
+++ src/ui/scenes/editor/GridUI.java
@@ -1,7 +1,6 @@
 package ui.scenes.editor;
 
 import editor.EditorController;
-import editor.EditorSidePanel;
 import javafx.scene.shape.Rectangle;
 import ui.GridPane;
 import javafx.scene.Node;
@@ -10,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.effect.ColorAdjust;
 import resources.properties.PropertiesUtilities;
 import ui.builder.UIBuilder;
+
 import java.util.ResourceBundle;
 
 
@@ -27,7 +27,7 @@ public class GridUI {
     private EditorSidePanel myItemMenu;
     private EditorController myController;
 
-    GridUI (Parent root, EditorSidePanel itemMenu, EditorController controller, ResourceBundle resources) {
+    GridUI(Parent root, EditorSidePanel itemMenu, EditorController controller, ResourceBundle resources) {
         myRoot = root;
         myItemMenu = itemMenu;
         myResources = resources;
@@ -39,7 +39,7 @@ public class GridUI {
      * Configures grid event handlers that allow the user to add and remove
      * objects from it.
      */
-    private void initGridControl () {
+    private void initGridControl() {
         myBuilder.addComponent(myRoot, myGridPane.getGroup());
         PropertiesUtilities util = new PropertiesUtilities();
         ColorAdjust hoverOpacity = new ColorAdjust();
@@ -61,27 +61,26 @@ public class GridUI {
         String swapPath = myResources.getString("swapPath");
         Node widthInputField =
                 myBuilder.addCustomTextField(myRoot, widthInputText, widthInputX, widthInputY,
-                                             widthInputWidth);
+                        widthInputWidth);
         Node heightInputField =
                 myBuilder.addCustomTextField(myRoot, heightInputText, heightInputX, heightInputY,
-                                             heightInputWidth);
+                        heightInputWidth);
         String updatePath = myResources.getString("updatePath");
         Node updateButton =
                 myBuilder.addCustomButton(myRoot, updatePath, updateX, updateY, updateWidth);
         updateButton.setOnMouseClicked(e -> {
             TextField xText = (TextField) widthInputField;
             TextField yText = (TextField) heightInputField;
-            try{
+            try {
                 int xInput = Integer.parseInt(xText.getText());
                 int yInput = Integer.parseInt(yText.getText());
                 myBuilder.removeComponent(myRoot, myGridPane.getGroup());
                 myGridPane.resizeReset(xInput, yInput);
-                myBuilder.addComponent(myRoot, myGridPane.getGroup());               
-            }
-            catch(Exception exc){
+                myBuilder.addComponent(myRoot, myGridPane.getGroup());
+            } catch (Exception exc) {
                 myBuilder.addNewAlert("Invalid Resize", "Please enter an inter value for row and column count");
             }
-                   
+
         });
         updateButton.setOnMouseEntered(e -> updateButton.setEffect(hoverOpacity));
         updateButton.setOnMouseExited(e -> updateButton.setEffect(null));
@@ -92,14 +91,14 @@ public class GridUI {
         swapButton.setOnMouseEntered(e -> swapButton.setEffect(hoverOpacity));
         swapButton.setOnMouseExited(e -> swapButton.setEffect(null));
     }
-    
-    public void loadGrid(){
+
+    public void loadGrid() {
         int colMax = myController.getCol();
         int rowMax = myController.getRow();
         myGridPane.loadReset(rowMax, colMax);
-        for(int i = 0; i < rowMax; i++){
-            for(int j = 0; j < colMax; j++){
-                System.out.println(myController.getBlock(i,j));
+        for (int i = 0; i < rowMax; i++) {
+            for (int j = 0; j < colMax; j++) {
+                System.out.println(myController.getBlock(i, j));
                 myGridPane.blockToGridPane(i, j, myController.getBlock(i, j));
 
             }
@@ -108,48 +107,52 @@ public class GridUI {
         myBuilder.addComponent(myRoot, myGridPane.getGroup());
     }
 
-    private void addGridBackground() {
-        Rectangle background = new Rectangle();
-        background.setLayoutX(14);
-        background.setLayoutY(43);
-        background.setWidth(621);
-        background.setHeight(621);
-        background.setId("border");
-        myBuilder.addComponent(myRoot,background);
+    /**
+     * Adds a border to the grid based on the grid's position and size
+     * by creating a Rectangle behind it
+     */
+    private void addGridBorder() {
+        PropertiesUtilities util = new PropertiesUtilities();
+        Rectangle border = new Rectangle();
+        border.setLayoutX(util.getIntProperty(myResources, "gridX") - util.getIntProperty(myResources, "borderSize"));
+        border.setLayoutY(util.getIntProperty(myResources, "gridY") - util.getIntProperty(myResources, "borderSize"));
+        border.setWidth(util.getIntProperty(myResources, "gridWidth") + util.getIntProperty(myResources, "borderSize") * 2);
+        border.setHeight(util.getIntProperty(myResources, "gridHeight") + util.getIntProperty(myResources, "borderSize") * 2);
+        border.setId("grid-border");
+        myBuilder.addComponent(myRoot, border);
     }
 
     /**
      * Creates a grid of specified width and height, and then adds
      * functionality to the grid
      */
-    public void initGrid (int gridWidth, int gridHeight) {
-        addGridBackground();
+    public void initGrid(int gridWidth, int gridHeight) {
+        addGridBorder();
         PropertiesUtilities util = new PropertiesUtilities();
-        myGridPane = new GridPane(
-                                  gridWidth,
-                                  gridHeight,
-                                  util.getIntProperty(myResources, "gridWidth"),
-                                  util.getIntProperty(myResources, "gridHeight"),
-                                  util.getIntProperty(myResources, "gridX"),
-                                  util.getIntProperty(myResources, "gridY"));
+        myGridPane = new GridPane(gridWidth,
+                gridHeight,
+                util.getIntProperty(myResources, "gridWidth"),
+                util.getIntProperty(myResources, "gridHeight"),
+                util.getIntProperty(myResources, "gridX"),
+                util.getIntProperty(myResources, "gridY"));
         myController.addGrid(gridHeight, gridWidth);
         initGridControl();
     }
 
-    /**
-     * Creates a grid and then adds functionality to it
-     */
-    public void initGrid () {
-        addGridBackground();
-        PropertiesUtilities util = new PropertiesUtilities();
-        myGridPane = new GridPane(
-                                  util.getIntProperty(myResources, "gridCellsWide"),
-                                  util.getIntProperty(myResources, "gridCellsHeight"),
-                                  util.getIntProperty(myResources, "gridWidth"),
-                                  util.getIntProperty(myResources, "gridHeight"),
-                                  util.getIntProperty(myResources, "gridX"),
-                                  util.getIntProperty(myResources, "gridY"));
-        initGridControl();
-    }
+//    /**
+//     * Creates a grid and then adds functionality to it
+//     */
+//    public void initGrid () {
+//        addGridBackground();
+//        PropertiesUtilities util = new PropertiesUtilities();
+//        myGridPane = new GridPane(
+//                                  util.getIntProperty(myResources, "gridCellsWide"),
+//                                  util.getIntProperty(myResources, "gridCellsHeight"),
+//                                  util.getIntProperty(myResources, "gridWidth"),
+//                                  util.getIntProperty(myResources, "gridHeight"),
+//                                  util.getIntProperty(myResources, "gridX"),
+//                                  util.getIntProperty(myResources, "gridY"));
+//        initGridControl();
+//    }
 
 }
---
+++ src/ui/scenes/engine/EngineSidePanelDELETE.java
@@ -0,0 +1,17 @@
+package ui.scenes.engine;
+
+import ui.builder.TabBuilderDELETE;
+
+public class EngineSidePanelDELETE extends TabBuilderDELETE {
+////	 private static final String SIDEPANEL_RESOURCES = "resources/sidepanel";
+//	 private Group myRegion;
+//	 private ResourceBundle myResources;
+//	 private UIBuilder myBuilder;
+//
+//	public EngineSidePanelDELETE(Group region, String resourceFileName){
+//		super(region, resourceFileName);
+//		myBuilder = new UIBuilder();
+//	}
+//
+	
+}
