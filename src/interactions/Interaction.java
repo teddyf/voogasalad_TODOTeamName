1f3a2a4fbb0aacdb242f9b1736c34adfb2c10b92
Filip Mazurek <fjm7@duke.edu>
2016-12-03 19:43:29 -0500

    Reinvents the ways in which interactions are used. Pending approval from the rest of the team.

--- src/interactions/Interaction.java
+++ src/interactions/Interaction.java
@@ -2,10 +2,20 @@ package interactions;
 
 import player.Player;
 
-
-public interface Interaction {
-    void actOnStep(Player player);
-    default void actOnTalk(String message){
+/**
+ * All interactions are to be used as classes that are going to be composed with board objects.
+ * These board objects will interact with the player as dictated by the interaction class(es)
+ * with which they are composed.
+ *
+ * The topmost and default method of interaction for all interactions. Whenever a child specific interaction does not
+ * implement the desired methods, they will default to not doing anything.
+ * @author Filip Mazurek
+ */
+public abstract class Interaction {
+    public void actOnStep(Player player) {
+        // do nothing
+    }
+    public void actOnTalk(String message){
         // do nothing
     }
 }
--- src/interactions/TeleportInteraction.java
+++ src/interactions/TeleportInteraction.java
@@ -6,7 +6,7 @@ import player.Player;
  * Allows the player to be teleported to a destination block from a teleporter.
  * @author Filip Mazurek
  */
-public class TeleportInteraction implements Interaction {
+public class TeleportInteraction extends Interaction {
     private int myDestinationRow;
     private int myDestinationCol;
 
