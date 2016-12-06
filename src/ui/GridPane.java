7e1279ec2d43bad05e40a98ecf825c2701c9fa67
Teddy Franceschi <ttf6@duke.edu>
2016-12-05 22:47:50 -0500

    sorry philipe

--- src/ui/GridPane.java
+++ src/ui/GridPane.java
@@ -273,7 +273,7 @@ public class GridPane {
     }
     
     public boolean buildLink(GridPaneNode node1, GridPaneNode node2, EditorController controller){
-        return controller.linkBlocks(node1.getRow(), node1.getCol(), node2.getRow(), node2.getCol(),0);
+        return controller.linkBlocks(node1.getRow(), node1.getCol(), node2.getRow(), node2.getCol(),0,0);
     }
     
 
