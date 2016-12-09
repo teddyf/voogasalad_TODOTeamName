63647e1cd18a527969aae7d0c8dd3f2ea984c9ff
Filip Mazurek <fjm7@duke.edu>
2016-12-08 23:33:34 -0500

    Adds more exceptions.

--- src/exceptions/NoPlayerException.java
+++ src/exceptions/NoPlayerException.java
@@ -4,15 +4,15 @@ package exceptions;
  * @author Filip Mazurek
  */
 
-public class NoPlayerException extends Exception {
+public class NoPlayerException extends Exception implements Alert {
 
     public NoPlayerException() {
         super();
 
     }
 
-//    @Override
-//    public String getMessage () {
-//        return String.format();
-//    }
+    @Override
+    public String getMessage () {
+        return String.format(NO_PLAYER);
+    }
 }
