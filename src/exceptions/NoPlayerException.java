528c58f5e74d9cfce4dfaf89ac0cf824b89928c2
Filip Mazurek <fjm7@duke.edu>
2016-12-08 23:02:35 -0500

    Adds custom exception framework, as well as one specific exception for testing.

---
+++ src/exceptions/NoPlayerException.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+/**
+ * @author Filip Mazurek
+ */
+
+public class NoPlayerException extends Exception {
+
+    public NoPlayerException() {
+        super();
+
+    }
+
+//    @Override
+//    public String getMessage () {
+//        return String.format();
+//    }
+}
