70dc4fe61c1b48f26c6d7fae9cbb7f073ad160eb
Robert Steilberg <rsteilberg@gmail.com>
2016-12-10 14:04:55 -0500

    start side menu

--- src/ui/scenes/editor/sidemenu/GridSideMenu.java
+++ src/ui/scenes/editor/sidemenu/GridSideMenu.java
@@ -1,8 +1,14 @@
 package ui.scenes.editor.sidemenu;
 
 import block.BlockType;
+import com.sun.javafx.scene.traversal.Direction;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.FlowPane;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,14 +27,24 @@ public class GridSideMenu extends SideMenu {
     }
 
 
+    private ScrollPane resizePane() {
+        FlowPane pane = new FlowPane();
 
+        ComboBox<Direction> directionComboBox = new ComboBox<>();
+        ObservableList<Direction> options = FXCollections.observableArrayList(Direction.values());
+        directionComboBox.setItems(options);
+
+        myBuilder.addComponent(pane, directionComboBox);
+
+        return new ScrollPane(pane);
+    }
 
     /**
      * Creates and adds tabs for each object type to the Item Menu
      */
     public void addTabs() {
-//        Tab tab = createTab("Resize", createResizePane());
-//        myPanel.getTabs().add(tab);
+        Tab tab = createTab("Resize", resizePane());
+        myPanel.getTabs().add(tab);
     }
 
 
