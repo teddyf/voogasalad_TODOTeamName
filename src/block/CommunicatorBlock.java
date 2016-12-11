7c0355bfb221e90a8123bcea84d783c43b37f4a4
Filip Mazurek <fjm7@duke.edu>
2016-12-11 00:45:59 -0500

    Reverts test files to normal

--- src/block/CommunicatorBlock.java
+++ src/block/CommunicatorBlock.java
@@ -15,7 +15,6 @@ public class CommunicatorBlock extends Block {
 
 	public CommunicatorBlock(String name, int row, int col) {
 		super(name, row, col);
-		setMessage("Waddup fuckers");
 	}
 
 	public void setMessage(String message) {
