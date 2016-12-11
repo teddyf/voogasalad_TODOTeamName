a179458a2d0b63460affc8c0e112bdc17c3135ea
Filip Mazurek <fjm7@duke.edu>
2016-12-11 00:26:18 -0500

    Begins framework for WinBlock

--- src/interactions/WinInteraction.java
+++ src/interactions/WinInteraction.java
@@ -1,7 +1,27 @@
 package interactions;
 
+import block.BlockUpdate;
+import block.BlockUpdateType;
+import player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @author Filip Mazurek, Aninda Manocha
+ * Interaction which triggers the successful end of the current game.
+ *
+ * @author Filip Mazurek,
  */
-public class WinInteraction {
+public class WinInteraction implements Interaction {
+
+    public WinInteraction() {
+
+    }
+
+    @Override
+    public List<BlockUpdate> act(Player player) {
+        List<BlockUpdate> updateList = new ArrayList<>();
+        updateList.add(new BlockUpdate(BlockUpdateType.WIN_GAME, player.getRow(), player.getCol(), ""));
+        return updateList;
+    }
 }
