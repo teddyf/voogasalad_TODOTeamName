adcaf315536c72aaf2b5d0d2e5d99e82b4e0e538
Filip Mazurek <fjm7@duke.edu>
2016-12-12 00:12:53 -0500

    temp

---
+++ src/interactions/ReceivePokemonInteraction.java
@@ -0,0 +1,16 @@
+package interactions;
+
+import block.BlockUpdate;
+import player.Player;
+
+import java.util.List;
+
+/**
+ * @author Filip Mazurek
+ */
+public class ReceivePokemonInteraction implements Interaction {
+
+    public List<BlockUpdate> act(Player player) {
+//        player.addP
+    }
+}
